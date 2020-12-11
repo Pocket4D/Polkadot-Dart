09c61917ff8f7adf359a4231b9333b6f84509f29
neeboo <ghostcorn@gmail.com>
2020-12-12 02:09:45 +0800

    fix(runTest):dont open directly on linux

--- scripts/runTest.sh
+++ scripts/runTest.sh
@@ -2,4 +2,12 @@
 
 flutter test --coverage test/polkadot_dart_test.dart
 genhtml -o coverage coverage/lcov.info
-open coverage/index-sort-l.html
+
+
+
+if [ "$(open coverage/index-sort-l.html)" = "" ] 
+  then
+    echo "Opened coverage/index-sort-l.html"
+else
+    echo "Use browser to open coverage/index-sort-l.html"
+fi
