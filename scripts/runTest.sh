d8af0534c57f657665a352ab839a44481cd77cb3
neeboo <ghostcorn@gmail.com>
2020-12-12 02:24:02 +0800

    fix(runTest):linux error

--- scripts/runTest.sh
+++ scripts/runTest.sh
@@ -3,11 +3,11 @@
 flutter test --coverage test/polkadot_dart_test.dart
 genhtml -o coverage coverage/lcov.info
 
-
-
-if [ "$(open coverage/index-sort-l.html)" = "" ] 
+SYSTEM=`uname -s`
+if [ "$SYSTEM" = "Darwin" ] 
   then
     echo "Opened coverage/index-sort-l.html"
+    open coverage/index-sort-l.html
 else
     echo "Use browser to open coverage/index-sort-l.html"
 fi
